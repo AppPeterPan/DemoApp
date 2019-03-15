3b19ac3bdd0d78d146b30b151751d219e64e6edb
peter <apppeterpan@gmail.com>
2019-03-16 00:01:03 +0800

    Add SecondViewController

---
+++ DemoApp/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  DemoApp
+//
+//  Created by SHIH-YING PAN on 2019/3/15.
+//  Copyright © 2019 SHIH-YING PAN. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
